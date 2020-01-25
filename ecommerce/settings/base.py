475ceaa0e02e2054ee682c897dea1f0c7b9311fa
zorojurio <zorojurio@gmail.com>
2020-01-25 20:04:53 +0530

    requirements.txt file created

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -151,8 +151,12 @@ MEDIA_ROOT = os.path.join(os.path.dirname(
 
 LOGOUT_REDIRECT_URL = '/login/'
 
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+CORS_REPLACE_HTTPS_REFERER = False
+HOST_SCHEME = "http://"
+SECURE_PROXY_SSL_HEADER = None
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SECURE_HSTS_SECONDS = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_FRAME_DENY = False
